21c43bcdc8c9a6b27660e2ed06ef2283c9468d3d
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-10-21 09:49:50 -0300

    cena com ui

---
+++ src/Assets/Scripts/LifeController.cs
@@ -0,0 +1,85 @@
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LifeController : MonoBehaviour {
+
+	private float startHealth = 4;
+	public float health;
+	private float maxHealth = 4;
+	public float lifePoints = 3;
+
+	private bool isDamageable;
+	Vector3 beginPos;
+	public Image healthGui;
+	public Text lifePointsText;
+	
+	public AudioClip[] audioClip;
+
+
+	void Start(){
+		health = startHealth;
+		isDamageable = true;
+		
+		beginPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+
+		UpdateView ();
+	}
+
+	void ApplyDamage(float damage){
+		
+		if (isDamageable){
+			health -= damage;
+		
+			UpdateView ();
+			health = Mathf.Max (0, health);
+			StartCoroutine(FlashingDamage ());
+			
+			if (health <= 0) {
+				lifePoints--;
+				RestartLevel ();
+			}
+
+			isDamageable = false;
+			Invoke ("ResetIsDamageable", 1.5f);
+		} 
+	}
+
+	void RestartLevel(){
+		health = startHealth;
+		UpdateView ();
+		transform.position = beginPos;
+	}
+
+	void ResetIsDamageable(){
+		isDamageable = true;
+	}
+
+	IEnumerator FlashingDamage(){
+		int i = 0;
+		while(i<5){
+			GetComponent<Renderer>().enabled = true;
+			yield return new WaitForSeconds(0.1f);
+			GetComponent<Renderer>().enabled = false;
+			yield return new WaitForSeconds(0.1f);
+			i++;
+		}
+		GetComponent<Renderer>().enabled = true;
+	}
+
+	void UpdateView(){
+		healthGui.fillAmount = health / maxHealth;
+		lifePointsText.text = lifePoints.ToString ();
+
+	}
+
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
