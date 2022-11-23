441818dc2327f4e3b5ca9bc7173a6d804a7f9b64
Lucas Miranda <lucasgmm@usp.br>
2022-11-23 20:09:01 -0300

    Separated start menu into its own scene

--- src/Assets/Scripts/StartGame.cs
+++ src/Assets/Scripts/StartGame.cs
@@ -1,13 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex	+ 1);
+    public void PlayButton(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("FirstStage");
     }
 }
